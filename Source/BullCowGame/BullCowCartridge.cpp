f5289b1229029778b2cc13594cf45400e0d080c0
Arthur <mtlgdroid@gmail.com>
2020-05-10 10:50:31 -0400

    check the length of input

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,10 +4,10 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(Text("The hidden word is: %s."),*HiddenWord);
+    PrintLine(TEXT("The hidden word is: %s."),*HiddenWord));
     PrintLine(TEXT("Welcome to Bull Cow!"));
     PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len());
-    PrintLine(TEXT("Enter your guess and press [ENTER] to continue...1;"))
+    PrintLine(TEXT("Enter your guess and press [ENTER] to continue...1;"));
     SetupGame();
     
 }
@@ -19,13 +19,20 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
      if(Input == HiddenWord)
      {
          PrintLine(TEXT("You guessed right! "));
+         //bGameOver = true;
 
      }else{
+         if(Input.Len() != HiddenWord.Len())
+            {
+                PrintLine(TEXT("The hidden word is %i character long, try again"), HiddenWord.Len());
+            }
          PrintLine(TEXT("It is wrong"));
+         //bGameOver = true;
      }
 }
 void UBullCowCartridge::SetupGame()
 {
     HiddenWord = TEXT("Honey"); // this is not input this is HiddenWord = "Honey"
     Lives = 4;
+    bGameOver = false;
 }

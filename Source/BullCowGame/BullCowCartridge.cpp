10d79d02c30d8f9e7f152a078d23a93a2028d825
Arthur <mtlgdroid@gmail.com>
2020-05-15 19:41:58 -0400

    Show the total number of words with NUM()

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+#include "HiddenWords.h"
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(TEXT("Hello it is me"));
-    PrintLine(TEXT("You need Macro TEXT to wrap the string"));
-    InitGame();
+    PrintLine(TEXT("There are %i words in the list"),Words.Num());
+    // PrintLine(TEXT("Hello it is me"));
+    // PrintLine(TEXT("You need Macro TEXT to wrap the string"));
+    // InitGame();
     
 }
 
